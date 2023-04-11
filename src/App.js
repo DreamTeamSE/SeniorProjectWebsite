f380cbd4f39ce9cd59ed5ab44b52b01cb16fd7b5
Emily Jones <emilyjones@Emilys-MacBook-Air.local>
2023-04-10 21:19:23 -0400

    added survey length to surveyjs data + framework for our own css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import 'survey-core/defaultV2.min.css';
+import './App.css';
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
-
-// Personal Hx Testing Referral Eligibility (Current)
-// const oldSurveyJson = {
-//   "surveyId": "7765f129-0ddd-4eee-8aa3-4c6de2f5c40c",
-//   "surveyPostId": "ef9e0428-0d08-4349-b31f-ed918614c025"
-// }
 
 // Personal Hx Testing Referral Eligibility (Experimental)
 const surveyJson = {
   "surveyId": "ca2e1aa0-e865-440b-9a5d-7452852d6a2a",
+  "surveyResultId": "f797bd01-c13b-453d-b9e1-8e6c6a6c1f90",
   "surveyPostId": "aca99aaa-d33f-45ba-b77f-bf2f85a430b3"
 }
 
 function App() {
   const survey = new Model(surveyJson);
+  survey.css = myCss;
+ 
   let timerStarted = false;
 
   survey.onAfterRenderQuestion.add(function() {
@@ -35,10 +33,10 @@ function App() {
     }
   })
 
-  survey.onComplete.add(function(){
-    survey.stopTimer();
-    timerStarted = false;
-    console.log("Time spent: ", survey.timeSpent);
+  survey.onComplete.add(function() {
+      survey.stopTimer();
+      timerStarted = false;
+      survey.getQuestionByName("surveytime").value = survey.timeSpent;
   });
 
   survey.onSendResult.add(function(sender, options){
@@ -48,7 +46,14 @@ function App() {
     }
   });
 
-  return <Survey model={survey}></Survey>
+  return <Survey model={survey}/>
 }
 
 export default App;
+
+var myCss = {
+  question: {
+    content: "content-text",
+    title: "question-text",
+  }
+};
