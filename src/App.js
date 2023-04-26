294f2431fa2978ae7617a48fbbba2b51327c0142
mjako <jakobmelendez@ufl.edu>
2023-04-26 16:31:12 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// personalHx and familyHx contain the json needed to render the surveys from SurveyJS
+// the full json's of each surveys can be found in surveys_raw from the base directory
 import personalHx from './personalHx.json';
 import familyHx from './familyHx.json';
 import RunSurvey from './RunSurvey';
 
 function App() {
   return (
+    // BrowserRouter allows for multiple web paths to be accessible from the homepage
+    // '/' or any invalid path will access the homepage, where the PersonalHx survey is rendered
+    // '/familyhx' will access the familyHx page, where the FamilyHx survey is rendered
     <BrowserRouter>
       <Routes>
         <Route exact path= "/" element={<RunSurvey title = "PersonalHx Survey" data={personalHx} />} />
--- src/RunSurvey.js
+++ src/RunSurvey.js
@@ -1,23 +1,27 @@
-import 'survey-core/defaultV2.min.css';
-import './App.css';
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 import React, { useEffect } from 'react';
+import 'survey-core/defaultV2.min.css';
+import './App.css';
 
+// handles the running of the survey
 function RunSurvey(props) {
+  // receives the survey json and survey name
   const surveyJson = props.data;
   const tabTitle = props.title;
 
+  // based on the passed in survey name in App.js the tab title will change to reflect it
   useEffect(() => {
     document.title = tabTitle;
   }, [tabTitle]); 
 
+  // creates a survey model from the passed in json
   const survey = new Model(surveyJson);
-  // survey.css = myCss;
-  
+
   let timerStarted = false;
   let results = [];
 
+  // whenever a question is rendered, check if the page is 1, if it is and the timer hasn't started, start it
   survey.onAfterRenderQuestion.add(function() {
     if (survey.currentPageNo === 1) {
       if (timerStarted === false) {
@@ -27,18 +31,21 @@ function RunSurvey(props) {
     }
   })
 
+  // whenever the current page is changing, if the user is back tracking clear their responses on the previous page
   survey.onCurrentPageChanging.add(function(sender, options) {
     if (options.isPrevPage === true) {
       options.oldCurrentPage.questions.forEach(function(question) {
         question.value = undefined;
       })
 
+      // additionally, check if the previous page was a result, and remove it from the result array
       if(options.oldCurrentPage.getQuestionByName("meetsCriteria") !== -1){
         results.splice(results.length - 1);
       }
     }
   })
 
+  // checks if the current page has a result presented on it
   function checkResultVisibility(resultName, options){
     if(options.newCurrentPage.questions.indexOf(survey.getQuestionByName(resultName)) !== -1 && survey.getQuestionByName(resultName).isVisible){
       return true;
@@ -47,6 +54,7 @@ function RunSurvey(props) {
     return false;
   }
 
+  // whenever a page is changed check if a result is visible, and then add it to the results array if it is
   survey.onCurrentPageChanged.add(function(sender, options) {
     if(checkResultVisibility("meetsCriteria", options)){
       results.push(1);
@@ -68,13 +76,16 @@ function RunSurvey(props) {
     }
   })
 
+  // handles the completion of the survey
   survey.onComplete.add(function() {
+      // stops the timer and stores the time in a hidden question in the survey so it can stored in the SurveyJS dashboard
       survey.stopTimer();
       timerStarted = false;
       survey.getQuestionByName("surveytime").value = survey.timeSpent;
 
       let resultText = "";
 
+      // builds a string to hold all of the result information
       for(let i = 0; i < results.length; i++){
         if(resultText.length !== 0){
           resultText += " and ";
@@ -100,9 +111,11 @@ function RunSurvey(props) {
         }
       }
 
+      // sets a hidden result question equal to the built result string so that it can be sent to the SurveyJS dashboard
       survey.getQuestionByName("endResult").value = resultText;
   });
 
+  // whenever a result is sent to SurveyJS, the survey needs to be reset and the results need to be emptied
   survey.onSendResult.add(function(sender, options){
     if(options.success){
       survey.clear();
@@ -115,10 +128,3 @@ function RunSurvey(props) {
 }
 
 export default RunSurvey;
-
-// var myCss = {
-//   question: {
-//     content: "content-text",
-//     title: "question-text",
-//   }
-// };
